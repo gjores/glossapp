402beb01e4887d1689a0e5025b4301693ec66d0e
Petter Gjöres <gjores@gmail.com>
2012-10-23 20:31:44 +0200

    initial glossapp commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gloss::Application.config.secret_token = 'fee3af180e9bbfd30e4c4924d0ab327e8b5d6d16fc16e58e4d0d11a5357ceb042620db91e708db5c6e3372ab8e42183b20192e66f6bea80248567d2fc404eb6e'
